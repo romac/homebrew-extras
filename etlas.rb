85e9a421f353128a69aecefcb9c987f0737c162e
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-04-02 16:03:34 +0200

    Add Etlas

---
+++ etlas.rb
@@ -0,0 +1,14 @@
+class Etlas < Formula
+  desc "Etlas, the build tool and package manager for the Eta programming language"
+  homepage "https://eta-lang.org/"
+  url "https://cdnverify.eta-lang.org/eta-binaries/etlas-1.5.0.0/binaries/x86_64-osx/etlas"
+  sha256 "c48307d1c504086365c8c444058693d9c0e364e44a1c6edd675089d9dc4467f6"
+
+  def install
+    bin.install "etlas"
+  end
+
+  test do
+    system "#{bin}/etlas"
+  end
+end
